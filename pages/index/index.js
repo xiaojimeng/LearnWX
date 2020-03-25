d81eb893f5afd04bdc6590caf61cfe31d6101eee
xiaojimeng <1484541126xjm@gmail.com>
2020-03-25 20:41:58 +0800

    07_wx:for

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,38 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    nowTime: new Date().toLocaleString(),
+    isActive: false,
+    isHave: false,
+    score: 30,
+    list:[
+      {id:0,name:"Kris"},
+      { id: 1, name: "Lorry" },
+      { id: 2, name: "Lisa" },
+      { id: 3, name: "Jam" },
+      { id: 4, name: "Jery" },
+
+    ]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
+  handleScroll(e) {
     console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  },
+  handleUpper() {
+    console.log('top/left')
+
+  },
+  handleLower() {
+    console.log('bottom/right')
+  },
+  onLoad() {
+    setInterval(() => {
+      this.setData({
+        nowTime: new Date().toLocaleString()
+      })
+    }, 1000)
+  },
+  handleColor() {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      isActive: !this.data.isActive
     })
   }
 })
