2620c123603c6c3d88f38f1353bd19c87d499ec3
xiaojimeng <1484541126@qq.com>
2020-04-05 12:09:56 +0800

    页面跳转

---
+++ pages/detail/detail.js
@@ -0,0 +1,57 @@
+// pages/detail/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    console.log(options)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+    // 获取首页的页面对象
+    const pages = getCurrentPages()
+    const index=pages[pages.length-2]
+    // 调用页面对象setData
+    index.setData({
+      title:'啦啦啦'
+    })
+  },
+
+  handleBack() {
+    wx.navigateBack({
+      
+    })
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,17 @@
 Page({
   data: {
-
+    title: '哈哈哈'
+  },
+  onLoad() {},
+  handleDatail() {
+    // wx.navigateTo({
+    //     url: '/pages/detail/detail'
+    //   })
+    wx.navigateTo({
+      url: '/pages/detail/detail?title="你好"'
+    })
+      // wx.redirectTo({
+      //   url: '',
+      // })
   }
 })
